928c1d146193e9f6a2551da84b21912845df6dc7
github username <github email address>
2020-10-20 15:21:50 -0500

    added modal for Login/signup

--- frontend/scripts/signUp.js
+++ frontend/scripts/signUp.js
@@ -1,64 +1,91 @@
-const renderSignUpForm = ( ) => {
-    console.log("We're signing up!")
+const renderSignUpForm = () => {
+  console.log("We're signing up!");
+  const title = document.querySelector('.title');
+  title.innerText = 'Signup';
+  const modalBody = document.querySelector('.modal-body');
 
-    changingContainer.innerHTML = ''
+  const signUpForm = document.createElement('form');
+  const nameLabel = document.createElement('label');
+  const userNameInput = document.createElement('input');
+  const passLabel = document.createElement('label');
+  const passwordInput = document.createElement('input');
+  const submit = document.createElement('button');
 
-    let signUpH3 = createEl('h3')
-    signUpH3.setAttribute('class', 'h3 mt-3')
-    signUpH3.innerText = "Create an Account"
+  signUpForm.className = 'user-form';
+  nameLabel.innerText = 'Username';
+  userNameInput.className = 'fInput';
+  userNameInput.type = 'text';
+  userNameInput.name = 'username';
+  userNameInput.setAttribute('placeholder', 'Username');
+  passLabel.innerText = 'Password';
+  passwordInput.className = 'fInput';
+  passwordInput.setAttribute('placeholder', 'Password');
+  passwordInput.type = 'password';
+  passwordInput.name = 'user';
+  submit.type = 'submit';
+  submit.innerText = 'Create Account';
 
-    let spacerDiv = document.createElement('div')
-    spacerDiv.setAttribute('style', 'height: 20px;')
+  signUpForm.append(nameLabel, userNameInput, passLabel, passwordInput, submit);
+  modalBody.append(signUpForm);
 
-    let bigSpacerDiv = document.createElement('div')
-    bigSpacerDiv.setAttribute('style', 'height: 30px;')
+  // changingContainer.innerHTML = ''
 
-    let changingContainerCol = createEl('div')
-    changingContainerCol.setAttribute('class', 'col text-center')
+  // let signUpH3 = createEl('h3')
+  // signUpH3.setAttribute('class', 'h3 mt-3')
+  // signUpH3.innerText = "Create an Account"
 
-    let signUpForm = createEl('form')
-    signUpForm.setAttribute('id', 'log-in-form')
+  // let spacerDiv = document.createElement('div')
+  // spacerDiv.setAttribute('style', 'height: 20px;')
 
-    let userNameDiv = createEl('div')
-    let userNameInput = createEl('input')
-    userNameInput.setAttribute('class', 'input mt-2')
-    userNameInput.setAttribute('placeholder', 'Username')
-    userNameInput.name = 'username'
-    userNameDiv.append(userNameInput)
+  // let bigSpacerDiv = document.createElement('div')
+  // bigSpacerDiv.setAttribute('style', 'height: 30px;')
 
-    let passwordDiv = createEl('div')
-    let passwordInput = createEl('input')
-    passwordInput.setAttribute('placeholder', 'Password')
-    passwordInput.setAttribute('class', 'input mt-2')
-    passwordInput.name = 'password'
-    passwordDiv.append(passwordInput)
+  // let changingContainerCol = createEl('div')
+  // changingContainerCol.setAttribute('class', 'col text-center')
 
-    let signUpBtn = createEl('button')
-    signUpBtn.setAttribute('class', 'btn-md btn-primary mt-2')
-    signUpBtn.innerText = 'Create Account'
+  // let signUpForm = createEl('form')
+  // signUpForm.setAttribute('id', 'log-in-form')
 
-    signUpForm.append(userNameDiv, passwordDiv, signUpBtn)
-    changingContainerCol.append(signUpH3, signUpForm)
-    changingContainer.append(changingContainerCol)
+  // let userNameDiv = createEl('div')
+  // let userNameInput = createEl('input')
+  // userNameInput.setAttribute('class', 'input mt-2')
+  // userNameInput.setAttribute('placeholder', 'Username')
+  // userNameInput.name = 'username'
+  // userNameDiv.append(userNameInput)
 
-    signUpForm.addEventListener('submit', function(e){
-        e.preventDefault()
+  // let passwordDiv = createEl('div')
+  // let passwordInput = createEl('input')
+  // passwordInput.setAttribute('placeholder', 'Password')
+  // passwordInput.setAttribute('class', 'input mt-2')
+  // passwordInput.name = 'password'
+  // passwordDiv.append(passwordInput)
 
-        console.log(e.target.username.value)
-        let userData = {
-            username: e.target.username.value,
-            password: e.target.password.value
-        }
-        let userOptions = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(userData)
-        }
-        fetch(urlPrefix + 'users', userOptions)
-        .then(res => res.json())
-        .then(user => renderHomePage(user))
-    })
-}
+  // let signUpBtn = createEl('button')
+  // signUpBtn.setAttribute('class', 'btn-md btn-primary mt-2')
+  // signUpBtn.innerText = 'Create Account'
+
+  // signUpForm.append(userNameDiv, passwordDiv, signUpBtn)
+  // changingContainerCol.append(signUpH3, signUpForm)
+  // changingContainer.append(changingContainerCol)
+
+  signUpForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    console.log(e.target.username.value);
+    let userData = {
+      username: e.target.username.value,
+      password: e.target.password.value
+    };
+    let userOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(userData)
+    };
+    fetch(urlPrefix + 'users', userOptions)
+      .then(res => res.json())
+      .then(user => renderHomePage(user));
+  });
+};
