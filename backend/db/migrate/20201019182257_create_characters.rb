698a573bcab14783e8c20378bc8110d4b9f5b7b5
denimcouch <alexmata92@ymail.com>
2020-10-20 16:30:59 -0500

    edited migrations and models

--- backend/db/migrate/20201019182257_create_characters.rb
+++ backend/db/migrate/20201019182257_create_characters.rb
@@ -1,9 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
-      t.integer :user_id
       t.string :name
-      t.string :role
       t.integer :hp
       t.integer :strength
       t.integer :speed
